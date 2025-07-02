cfbea4fdb60c9c88dd4017698d6f82b21603635f
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-07-02 15:20:06 +0800

    adding logo

--- src/App.jsx
+++ src/App.jsx
@@ -88,8 +88,8 @@ const App = () => {
   return (
     <main>
       <div className="pattern"/>
-
       <div className="wrapper">
+        <img src="./logo.png" className='w-32' alt="Logo icon" />
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
